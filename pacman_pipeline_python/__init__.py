d626d0119e6b23fa9a896c730e458a133a7aaf70
Najja Marshall <najja.marshall@gmail.com>
2020-11-11 18:50:00 -0500

    Updated __init__ (explicit definition of dj.config)

--- pacman_pipeline_python/__init__.py
+++ pacman_pipeline_python/__init__.py
@@ -4,7 +4,11 @@ from os.path import dirname, basename, isfile, join
 from dotenv import load_dotenv, find_dotenv
 
 # load env file
-load_dotenv(join(dirname(__file__), '..', '.env')) 
+load_dotenv(join(dirname(__file__), '..', '.env'))
+
+dj.config['database.host'] = os.getenv('DJ_HOST')
+dj.config['database.user'] = os.getenv('DJ_USER')
+dj.config['database.password'] = os.getenv('DJ_PASS')
 
 # write mode to database prefix (only for sandbox installation)
 dj.config['database.prefix'] = (os.getenv("MODE") + '_' \
