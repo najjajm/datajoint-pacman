a44c92cc4856d79f5f1fdaf2e4f2bae1926c3acb
Najja Marshall <najja.marshall@gmail.com>
2020-11-28 18:57:14 -0500

    Added pacman_brain.NeuronPsth.plot_heat function

--- pacman_pipeline_python/pacman_brain.py
+++ pacman_pipeline_python/pacman_brain.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import neo
 import matplotlib.pyplot as plt
+import matplotlib.colors as mplcol
 from churchland_pipeline_python import lab, acquisition, processing
 from churchland_pipeline_python.utilities import datajointutils
 from . import pacman_acquisition, pacman_processing
@@ -167,3 +168,139 @@ class NeuronPsth(dj.Computed):
 
         # insert neuron PSTH
         self.insert1(key)
+
+    # =================
+    def plot_heat(
+        self, 
+        soft_normalize: int=5, 
+        z_score: bool=False, 
+        condition_sort_idx=0,
+        fig_size=(24, 14),
+        color_map='plasma'
+        ):
+
+        # identify common conditions
+        common_conditions = (dj.U('condition_id').aggr(self, count='count(*)') \
+            & 'count={}'.format(len(processing.Neuron & self))).proj()
+
+        # fetch sorted condition keys
+        condition_keys = (pacman_acquisition.ConditionParams \
+            * pacman_acquisition.ConditionParams().proj_label(n_sigfigs=2) \
+            * pacman_acquisition.ConditionParams().proj_rank() \
+            & common_conditions) \
+                .fetch('KEY', order_by='condition_rank')
+
+        # fetch condition time and force vectors
+        condition_attr = [(pacman_acquisition.Behavior.Condition & cond_key & self) \
+            .fetch('condition_time', 'condition_force', limit=1, as_dict=True)[0] \
+            for cond_key in condition_keys]
+
+        # fetch neuron keys
+        unit_keys = (processing.Neuron & self).fetch('KEY', order_by=['session_date', 'neuron_id'])
+
+        # fetch psths and stack across units
+        psths = [np.stack((self & cond_key).fetch('neuron_psth', order_by=['session_date', 'neuron_id'])) \
+            for cond_key in condition_keys]
+
+        # soft-normalize
+        if soft_normalize is not None:
+
+            psth_range = np.hstack(psths).ptp(axis=1, keepdims=True)
+            psths = [X/(psth_range + 5) for X in psths]
+
+        # z-score
+        if z_score:
+
+            psth_mean = np.hstack(psths).mean(axis=1, keepdims=True)
+            psth_std = np.hstack(psths).std(axis=1, ddof=0, keepdims=True)
+            psths = [(X-psth_mean)/psth_std for X in psths]
+
+        # firing rate range across conditions
+        psth_ranges = np.array([(X.min().min(), X.max().max()) for X in psths])
+        psth_range_cross_cond = (int(np.floor(psth_ranges[:,0].min())), int(np.ceil(psth_ranges[:,1].max())))
+
+        # plot order
+        psth_amplitude = psths[condition_sort_idx].sum(axis=1)
+        unit_idx = np.arange(psths[0].shape[0])
+        unit_order = [iidx for x, iidx in sorted(zip(psth_amplitude, unit_idx), reverse=True)]
+
+        # time range and width ratios
+        cmap_prop = 0.25
+        time_range = [x['condition_time'].ptp() for x in condition_attr]
+        time_range = time_range + [np.array(time_range).min() * cmap_prop]
+        width_ratios = time_range / sum(time_range)
+
+        # setup figure
+        n_rows = 2
+        n_columns = 1 + len(condition_keys)
+
+        fig = plt.figure(figsize=fig_size, constrained_layout=True)
+        gs = fig.add_gridspec(ncols=n_columns, nrows=n_rows, width_ratios=width_ratios, height_ratios=[0.075, 0.925])
+
+        # set colormap
+        if isinstance(color_map, str):
+            cmap = color_map
+        else:
+            cmap = mplcol.LinearSegmentedColormap.from_list('cmap', color_map)
+
+        for nd_idx in np.ndindex(n_rows, n_columns):
+
+            if nd_idx[1] < n_columns-1:
+
+                # make subplot
+                ax = fig.add_subplot(gs[nd_idx])
+            
+                # condition time vector
+                t = condition_attr[nd_idx[1]]['condition_time']
+
+                # x-tick position indices
+                x_tick_pos = [iidx for iidx, ti in enumerate(t) if ti==round(ti)]
+                x_tick_vals = ['{}'.format(int(x)) for x in t[x_tick_pos]]
+
+                if len(x_tick_pos) > 3:
+                    x_tick_pos = x_tick_pos[1:-1]
+                    x_tick_vals = x_tick_vals[1:-1]
+                
+                if nd_idx[0] == 0:
+
+                    # plot condition force profile
+                    ax.plot(t, condition_attr[nd_idx[1]]['condition_force'], c='k')
+
+                    # format axes
+                    ax.set_xlim(t[[0, -1]])
+                    ax.set_ylim([0, 20])
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+                    [ax.spines[edge].set_visible(False) for edge in ['top','right','bottom','left']]
+
+                else:
+                    # plot heatmap
+                    im = ax.imshow(
+                        psths[nd_idx[1]][unit_order,:], 
+                        aspect='auto', 
+                        cmap=cmap, 
+                        vmin=(0 if soft_normalize is not None else psth_range_cross_cond[0]), 
+                        vmax=(1 if soft_normalize is not None else psth_range_cross_cond[1])
+                    )
+
+                    # format axes
+                    ax.set_xticks(x_tick_pos)
+                    ax.set_xticklabels(x_tick_vals, fontsize='large')
+                    ax.set_yticks([])
+                    ax.set_xlabel('time (s)', fontsize='xx-large')
+
+            elif nd_idx[1] == n_columns-1 and nd_idx[0] == 1:
+
+                ax = fig.add_subplot(gs[nd_idx])
+
+        cbar = fig.colorbar(im, cax=ax);
+        cbar.ax.tick_params(labelsize='large')
+
+        fig.show()
+
+        # return sorted unit keys
+        return fig, np.array(unit_keys)[unit_order]
+        
+
+        
+
