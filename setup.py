45588426275dcae632a31d082937d44099e870b8
Najja Marshall <najja.marshall@gmail.com>
2020-12-20 23:03:52 -0500

    Updated setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
+min_py_version = (3, 5)
+
+if sys.version_info <  min_py_version:
+    sys.exit('DataJoint is only supported for Python {}.{} or higher'.format(*min_py_version))
+
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'requirements.txt')) as f:
