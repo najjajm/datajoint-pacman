dc0e5779e55aacfc6198e8b158cc8c530a4d93cc
Najja Marshall <najja.marshall@gmail.com>
2021-05-21 13:26:05 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'requirements.txt')) as f:
 
 setup(
     name='datajoint-pacman',
-    version='1.1.2',
+    version='1.1.3',
     description='Datajoint schemas for PacMan Task',
     author='Najja Marshall',
     author_email='njm2149@columbia.edu',
